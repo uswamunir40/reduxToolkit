093015bd6f02d55bbca18ee0e0a4c429f3547f2f
uswamunir40 <uswa40munir@gmail.com>
2024-08-05 22:29:09 +0500

    git initialized

---
+++ reduxToolkitBackend/src/db/config.js
@@ -0,0 +1,33 @@
+import { Sequelize } from 'sequelize';
+
+
+const dbName = process.env.DB_NAME;
+const dbUserName = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+
+
+
+console.log(dbName, dbPassword, dbUserName, dbHost, " dbName, dbPassword,dbUsername,dbHost,")
+
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
+    host: dbHost,
+    dialect: "postgres",
+    logging: console.log,
+  });
+
+
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+export { connectDB };
+export default sequelize;
